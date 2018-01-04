b3b41406d2a5755ebd5c623f0c6933560bb8fd91
André J. Kelling <kontakt@andrekelling.de>
2018-01-04 16:49:36 +0100

    fix wrong fix sw install with async _._

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,34 @@
 var dataCacheName = 'weatherData-v1';
-var cacheName = 'weatherPWA-v1';
+var cacheName = 'weatherPWA-v3';
 var filesToCache = [
-    '/',
-    '/index.html',
-    '/scripts/app.js',
-    '/inline.css',
-    '/images/clear.png',
-    '/images/cloudy-scattered-showers.png',
-    '/images/cloudy.png',
-    '/images/fog.png',
-    '/images/ic_add_white_24px.svg',
-    '/images/ic_refresh_white_24px.svg',
-    '/images/partly-cloudy.png',
-    '/images/rain.png',
-    '/images/scattered-showers.png',
-    '/images/sleet.png',
-    '/images/snow.png',
-    '/images/thunderstorm.png',
-    '/images/wind.png'
+    './',
+    './index.html',
+    './scripts/app.js',
+    './inline.css',
+    './images/clear.png',
+    './images/cloudy-scattered-showers.png',
+    './images/cloudy.png',
+    './images/fog.png',
+    './images/ic_add_white_24px.svg',
+    './images/ic_refresh_white_24px.svg',
+    './images/partly-cloudy.png',
+    './images/rain.png',
+    './images/scattered-showers.png',
+    './images/sleet.png',
+    './images/snow.png',
+    './images/thunderstorm.png',
+    './images/wind.png'
   ];
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
-  e.waitUntil(async function() {
+
+  e.waitUntil(
     caches.open(cacheName).then(function(cache) {
         console.log('[ServiceWorker] Caching app shell');
         return cache.addAll(filesToCache);
-      })
-  });
+    })
+  );
 });
 
 self.addEventListener('activate', function(e) {
